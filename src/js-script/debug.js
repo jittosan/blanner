1e2ff118d0869cf9f93eb613227ad26320ceef29
ziyi105 <ahziyi@gmail.com>
2023-01-15 04:57:52 +0800

    read csv file

--- src/js-script/debug.js
+++ src/js-script/debug.js
@@ -2,10 +2,4 @@ import {Module} from './module.js';
 import {ModPool} from './modules.js';
 import { ModuleContainer } from './modules.js';
  
-var module = new Module("ESP1111");
-
-console.log(module.getModuleCode());
-
-var modPool = new ModPool([new Module("ESP1111"), new Module("ESP2111"), new Module("ESP2110")]);
-modPool.addModule(new Module("ESP3111"));
-console.log(modPool.getModuleList());
+var modPool = new ModPool("master.csv");
